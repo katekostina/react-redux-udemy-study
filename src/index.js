d072530d0bd47022161eaaa88b05cf4b5d3169ad
Kate Kostina <katekostina@yahoo.com>
2022-01-28 20:26:30 +0300

    Create app in create-react-app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <span>Hi there!</span>
+  );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
